4c6d0a651102e03b12a653fb8ac42312b9f78ca5
Bin Le <bin.le.code@gmail.com>
2019-04-12 15:02:09 -0400

    more unbounded stream example

--- src/main/java/stream/TestStreamUnbounded.java
+++ src/main/java/stream/TestStreamUnbounded.java
@@ -1,18 +1,27 @@
 package stream;
 
 import java.util.List;
+import java.util.function.IntSupplier;
+import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class TestStreamUnbounded {
 
     public static void main(String[] args) {
 
-        streamFromIterate();
-
+//        streamFromIterate();
+        streamFromGenerate();
     }
 
+
+    /**
+     * Stream.iterate() is a common way of building source unbounded stream of element by
+     * defining the element with seed value and iterating logic (unary operator functional
+     * interface).
+     */
     public static void streamFromIterate() {
 
         System.out.println(
@@ -49,6 +58,55 @@ public class TestStreamUnbounded {
             }
         }).takeWhile(n -> n < 1000).collect(Collectors.toList());
         System.out.println(fiboList2);
+    }
+
+    /**
+     * Stream.generate lets you produce an infinite stream of values computed on demand.
+     * But generate doesn’t apply successively a function on each new produced value.
+     */
+    public static void streamFromGenerate() {
+
+
+        // generate sequence in state-less mode using lambda or method reference
+        System.out.println(
+                Stream.generate(Math::random)
+                        .limit(5)
+                        .collect(Collectors.toList())
+        );
+
+        // generate sequence leveraging some insternal state, use inline function implementation
+        System.out.println(
+                Stream.generate(new Supplier<Integer>() {
+                    private int seed = 0;
+                    @Override
+                    public Integer get() {
+                        return seed++;
+                    }
+                })
+                        .takeWhile(n -> n < 5)
+                        .collect(Collectors.toList())
+        );
+
+        // internal state between iterations of sequence generation sometimes is a must, like fibonacci
+        System.out.println("fibonacci below 1000: " +
+                IntStream.generate(new IntSupplier() {
+                    // here two internal states for previous two fibo numbers
+                    int ppre = 0;
+                    int pre = 1;
+
+                    @Override
+                    public int getAsInt() {
+                        int fibo = ppre;  // initial ppre value (0) represents the first fibonacci number
+                        int n = ppre + pre;  // calculate new fibo from previous two fibo
+                        ppre = pre;
+                        pre = n;
+
+                        return fibo;
+                    }
+                }).takeWhile(n -> Integer.valueOf(n) < 1000)
+                        .boxed()
+                        .collect(Collectors.toList())
+        );
     }
 
 }
