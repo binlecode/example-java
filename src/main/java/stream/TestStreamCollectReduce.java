e0d793353a321654f33288fcded2e5cb7ece8cb3
Bin Le <bin.le.code@gmail.com>
2019-04-13 18:17:43 -0400

    more stream collector examples

---
+++ src/main/java/stream/TestStreamCollectReduce.java
@@ -0,0 +1,120 @@
+package stream;
+
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+
+/**
+ * The collect method on a stream triggers a reduction operation (parameterized by a Collector) on
+ * the elements of the stream.
+ */
+public class TestStreamCollectReduce {
+
+    public static void main(String[] args) {
+
+        streamMaxMinSum();
+        collectorReducingAndStreamReduce();
+    }
+
+
+
+    public static void streamMaxMinSum() {
+        System.out.println("max of int stream: " +
+                IntStream.generate(() -> (int)(Math.random() * 100))
+                        .distinct()
+                        .limit(10)
+                        .boxed()
+                        .collect(Collectors.maxBy(Integer::compareTo)) // or .minBy(..)
+                        .orElse(0)
+        );
+
+        System.out.println("sum of double stream: " +
+                Stream.generate(Math::random)
+                        .limit(10)
+                        .collect(Collectors.summingDouble(Double::doubleValue))
+        );
+        // the Collectors.summingDouble(...) can be simplified by .sum() method
+        System.out.println("sum of int stream: " +
+                IntStream.generate(() -> (int)(Math.random() * 100))
+                        .distinct()
+                        .limit(10)
+                        .sum()
+        );
+
+        System.out.println("average of double stream: " +
+                DoubleStream.generate(Math::random)
+                .limit(10)
+                .average()
+                .orElse(0)
+        );
+
+        // now all above combined into one convenient summerizing method that provides
+        // count, sum, max, min, and average in SummaryStatistics object
+        System.out.println("summary of double stream: " +
+                Stream.generate(Math::random)
+                .limit(10)
+                .collect(Collectors.summarizingDouble(Double::doubleValue))
+        );
+    }
+
+    /**
+     * The Collectors.reducing factory method is a generalization of stream reducing support.
+     * For most cases, the same logic can be implemented via Stream.reduce functional API.
+     */
+    public static void collectorReducingAndStreamReduce() {
+        // the Collectors.summing method is essentially doing:
+        System.out.println("summing with Collectors.reducing: " +
+                DoubleStream.generate(Math::random)
+                        .limit(10)
+                        .boxed()
+        //                .collect(Collectors.reducing(Double::sum))
+                        .collect(Collectors.reducing((a, b) -> a + b))
+                        .orElse(0d)
+        );
+
+        // this summing logic can be rewritten using Stream.reduce() method
+        System.out.println("summing with Stream.reduce: " +
+                Stream.generate(() -> (int)(Math.random() * 100))
+                        .limit(10)
+                        .reduce((a, b) -> a + b)
+                        .orElse(0)
+        );
+        // the result Optional object can be replaced by providing a seed variable for the reduce function
+        System.out.println("summing with Stream.reduce with seed: " +
+                Stream.generate(() -> (int)(Math.random() * 100))
+                        .limit(10)
+                        .reduce(0, (a, b) -> a + b)
+        );
+
+        // now doing comparision reducing for max / min value
+        System.out.println("max with Collectors.reducing: " +
+                DoubleStream.generate(Math::random)
+                        .limit(10)
+                        .boxed()
+                        .collect(Collectors.reducing((a, b) -> a > b ? a : b))
+                        .orElse(null)
+        );
+        // or using Stream.reduce() with BinaryOperator functional interface
+        System.out.println("min with Stream.reduce: " +
+                DoubleStream.generate(Math::random)
+                        .limit(10)
+                        .boxed()
+                        .reduce((a, b) -> a < b ? a : b)
+                        .orElse(null)
+        );
+    }
+
+    /**
+     * You may wonder the difference b/t Stream.reduce(..) vs Stream.collect(Collectors.reducing(..)).
+     */
+    public static void collectorReducingVsStreamReduce() {
+
+
+    }
+
+
+
+
+}
---
+++ src/main/java/stream/TestStreamGrouping.java
@@ -0,0 +1,93 @@
+package stream;
+
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class TestStreamGrouping {
+
+    public static void main(String[] args) {
+
+//        simpleCollectorGrouping();
+
+//        multiLevelGrouping();
+        partitioning();
+    }
+
+    public static void simpleCollectorGrouping() {
+        System.out.println(
+                Stream.generate(() -> (int)(Math.random() * 100))
+                        .distinct()
+                        .limit(20)
+                        .peek(System.out::println)  // just for logging internal element state
+                        .collect(Collectors.groupingBy(n -> n % 2))  // group by odd/even
+        );
+
+        // group by custom categorization
+        Stream.generate(() -> (int)(Math.random() * 100))
+                .distinct()
+                .limit(20)
+                .collect(Collectors.groupingBy(n -> {
+                            if (n < 20) return "low";
+                            else if (n > 100) return "crazy"; // wont find one
+                            else if (n > 90) return "very-high";  // case check ordering matters
+                            else if (n > 80) return "high";
+                            else return "medium";
+                        },
+                        Collectors.counting()  // apply counting, which turns groupBy to countBy
+                ))
+                .forEach((k, v) -> System.out.println(k + " => count: " + v)); // print consuming map k v pair
+    }
+
+    public static void multiLevelGrouping() {
+        Stream.generate(() -> (int)(Math.random() * 100))
+                .limit(100)
+                .distinct()
+                .collect(Collectors.groupingBy(   // 1st level
+                        n -> n % 2,
+                        Collectors.groupingBy(n -> {  // 2nd level
+                                    if (n < 20) return "low";
+                                    else if (n > 90) return "very-high";
+                                    else if (n > 80) return "high";
+                                    else return "medium";
+                            }, Collectors.groupingBy(n -> {  // 3rd level, can nest on and on
+                                    if (n.toString().endsWith("3")) return "3-ending";
+                                    else return "others";
+                            }, Collectors.counting()) // transform final sub list to count
+                        )
+                ))
+                .forEach((k, v) -> {
+                    System.out.println(k % 2 == 0 ? "even:" : "odd:");
+                    v.forEach((k2, v2) -> {
+                        System.out.println("\t" + k2);
+                        v2.forEach((k3, v3) -> {
+                            System.out.println("\t\t" + k3 + ": " + v3);
+                        });
+                    });
+                } );
+    }
+
+
+    /**
+     * Partitioning is a special case of grouping: having a predicate called a partitioning function as
+     * a classification function.
+     * The fact that the partitioning function returns a boolean means the resulting grouping Map will
+     * have a Boolean as a key type, and therefore, there can be at most two different groups—one for
+     * true and one for false.
+     */
+    public static void partitioning() {
+        System.out.println("Is number even:");
+        Stream.generate(() -> (int)(Math.random() * 100))
+                .distinct()
+                .limit(10) // get 10 distinct integers
+                .collect(Collectors.partitioningBy(
+                        n -> n % 2 == 0,
+                        Collectors.collectingAndThen(  // takes two functions
+                                Collectors.summingInt(Integer::intValue),  // get sum for sub group values
+                                Integer::intValue)   // simply collect the result
+                ))
+                .forEach((k, v) -> System.out.println(k + " : " + v));
+    }
+
+}
