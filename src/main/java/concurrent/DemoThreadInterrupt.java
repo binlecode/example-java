bd804c42e54eb790e3d3f927b1c3e686dadb3202
Bin Le <bin.le.code@gmail.com>
2019-04-24 10:02:47 -0400

    tread example update wip

--- src/main/java/concurrent/DemoThreadInterrupt.java
+++ src/main/java/concurrent/DemoThreadInterrupt.java
@@ -5,46 +5,52 @@ package concurrent;
  */
 public class DemoThreadInterrupt {
 
-    static void threadMessage(String message) {
-        System.out.format("%s: %s%n", Thread.currentThread().getName(), message);
-    }
-
     private static class MessageLoop implements Runnable {
         @Override
         public void run() {
             try {
                 for (int i = 0; i < 200; i++) {
                     Thread.sleep(1000);
-                    System.out.println(">> message loop is running round #" + i);
+                    threadMessage(">> message loop is running round #" + i);
                 }
             } catch (InterruptedException e) {
-                System.out.println(">> message loop was interrupted");
+                threadMessage(">> message loop was interrupted");
             }
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        threadMessage("Starting MessageLoop thread");
+    public static void main(String[] args) {
+        //fixme: this try-catch won't work as it is not able catch cli terminal ctrl-c
+        //fixme: need to define Runtime shutdown hook
+        try {
+            threadMessage("Starting MessageLoop thread");
 
-        Thread t = new Thread(new MessageLoop());
-        t.start();
-        threadMessage("Waiting for MessageLoop thread to finish");
+            Thread t = new Thread(new MessageLoop());
+            t.start();
+            threadMessage("Waiting for MessageLoop thread to finish");
 
-        // wait until MessageLoop thread exits
-        while (t.isAlive()) {
-            // join is dependent on the OS for timing, so you should not
-            // assume that join will wait exactly as long as you specify
-            t.join(1000);
-            threadMessage("I am waiting ...");
-            Thread.sleep(5000);
-            if (t.isAlive()) {
-                threadMessage("Tired of waiting");
-                t.interrupt();
-                t.join();
+            // wait until MessageLoop thread exits
+            while (t.isAlive()) {
+                // join is dependent on the OS for timing, so you should not
+                // assume that join will wait exactly as long as you specify
+                t.join(1000);
+                threadMessage("I am waiting ...");
+                Thread.sleep(5000);
+                if (t.isAlive()) {
+                    threadMessage("Tired of waiting");
+                    t.interrupt();
+                    t.join();
+                }
             }
-        }
 
-        threadMessage("Finally done");
+            threadMessage("Finally done");
+        } catch (Exception ie) {
+            threadMessage("Got interrupted, abort");
+        }
+    }
+
+    static void threadMessage(String message) {
+        System.out.format("%s: %s%n", Thread.currentThread().getName(), message);
     }
 
 }
